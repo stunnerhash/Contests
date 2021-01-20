f7b62bc383f9e7405d2bceba4898b2d8fb472f3f
stunnerhash <utyadav07860@gmail.com>
2021-01-21 01:26:45 +0530

    codeforces round 696  atcoders keneyence 2021

---
+++ CodeForces/round-696 Div2/B_Different_Divisors.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define loop for(int i = 0; i < n; i++) 
+#define lop(i, n) for (int i = 0; i < n; i++) 
+#define lp(i, k, n) for (int i = k; k < n? i < n: i > n; k < n? i += 1: i -= 1) 
+#define dbg(x) cout << #x << "=" << x << endl
+#define dbug(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define clr(x) memset(x, 0, sizeof(x))
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define trav(a) for (auto it = a.begin(); it != a.end(); it++)
+#define endl "\n"
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '"' << x << '"';}
+void __print(const string &x) {cerr << '"' << x << '"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+bool prime[50001];
+void SieveOfEratosthenes(){
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= 50001; p++)    {
+        if (prime[p] == true)
+            for (int i = p * p; i <= 50001; i += p)
+                prime[i] = false;
+    }
+}
+void Solve()
+{
+  int d;
+  cin>>d;
+  long long ans;
+    vector<int> a;
+    int i =1;
+    while(a.size()<3){
+        while(1){
+            if(prime[i]){
+                a.pb(i);
+                break;
+            }
+            i++;
+        }
+        i+=d;
+    }
+    debug(a);
+    ans = a[0]*a[1]*a[2];
+    cout<<ans<<endl;
+}
+
+int main()
+{
+ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+ int Testcase = 1;
+ cin >> Testcase;
+ SieveOfEratosthenes();
+ while (Testcase--) Solve();
+ return 0;
+}
---
+++ graph.cpp
@@ -0,0 +1,64 @@
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+class Graph
+{
+    int V;
+    list<int> *adj;
+    void DFSUtil(int v, bool visited[]);
+
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void DFS(int v);
+    void BFS(int s);
+};
+
+Graph::Graph(int V){
+    this->V = V;
+    adj = new list<int>[V];
+}
+void Graph::addEdge(int v, int w){
+    adj[v].push_back(w);
+}
+void Graph::BFS(int s)
+{
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+    list<int> queue;
+    visited[s] = true;
+    queue.push_back(s);
+
+    list<int>::iterator i;
+
+    while (!queue.empty())
+    {
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+        for (i = adj[s].begin(); i != adj[s].end(); ++i)
+            if (!visited[*i]){
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+    }
+}
+void Graph::DFSUtil(int v, bool visited[])
+{
+    visited[v] = true;
+    cout << v << " ";
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFSUtil(*i, visited);
+}
+void Graph::DFS(int v)
+{
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+    DFSUtil(v, visited);
+}
