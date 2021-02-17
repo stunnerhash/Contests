9a12d8d79b957adc97fc5c433d3a66343c24bd8b
stunnerhash <utyadav07860@gmail.com>
2021-02-17 16:19:03 +0530

    codeforces educational 104

---
+++ CodeForces/Educational-104 Div2/B_Cat_Cycle.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define loop for(int i = 0; i < n; i++) 
+#define lop(i, n) for (int i = 0; i < n; i++) 
+#define lp(i, k, n) for (int i = k; k < n? i < n: i > n; k < n? i += 1: i -= 1) 
+#define dbg(x) cout << #x << "=" << x << endl
+#define dbug(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+#define read(type) readInt<type>()
+#define pb push_back
+#define mp make_pair
+#define clr(x) memset(x, 0, sizeof(x))
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define trav(a) for (auto it = a.begin(); it != a.end(); it++)
+#define endl "\n"
+#define INF (int)1e9
+#define EPS 1e-9
+#define mod 1000000007
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '"' << x << '"';}
+void __print(const string &x) {cerr << '"' << x << '"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+void Solve()
+{
+    int n , k ;
+    cin>>n>>k;
+    if(n%2 == 0 ) {
+        int ans =(k%n)? k%n: n;
+        cout<<ans<<endl;
+        return ;
+    }
+    int shouldbe= k%n;
+    int newk = n/2;
+    int additional = (k-1)/newk;
+    debug(newk,additional, shouldbe);
+    ll ans = (shouldbe+additional)%n;
+    cout<<(ans? ans: n)<<endl;
+}
+
+int main()
+{
+ ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+ int Testcase = 1;
+ cin>>Testcase;   
+ while (Testcase--)Solve();
+ return 0;
+}
