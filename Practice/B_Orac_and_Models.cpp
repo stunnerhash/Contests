926e4e6743d188a713bb1c37f8901565934e6dd5
stunnerhash <utyadav07860@gmail.com>
2021-06-27 19:55:55 +0530

    deleting a2Oj files

--- Practice/B_Orac_and_Models.cpp
+++ Practice/B_Orac_and_Models.cpp
@@ -5,26 +5,34 @@ using namespace std;
 #define loop for(int i = 0; i < n; i++) 
 #define lop(i, n) for (int i = 0; i < n; i++) 
 #define lp(i, k, n) for (int i = k; k < n? i < n: i > n; k < n? i += 1: i -= 1) 
-#define dbg(x) cout << #x << "=" << x << endl
-#define dbug(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
-#define read(type) readInt<type>()
-#define pb push_back
-#define no             {cout << "NO" <<endl; return;}
-#define mp make_pair
+#define trav(a) for (auto it = a.begin(); it != a.end(); it++)
+#define yes(flag) cout<<(flag ?"YES":"NO")<<endl;
+#define no { cout<<"NO"<<endl; return;}
 #define clr(x) memset(x, 0, sizeof(x))
 #define all(x) x.begin(), x.end()
 #define sortall(x) sort(all(x))
-#define trav(a) for (auto it = a.begin(); it != a.end(); it++)
-#define endl "\n"
+#define pb push_back
+#define mp make_pair
 #define INF (int)1e9
+#define endl "\n"
+#define mod 1e9+7
 #define EPS 1e-9
-#define mod 1000000007
+#define F first
+#define S second
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pl;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<pi> vpi;
 typedef vector<vi> vvi;
+
+// #define read(type) readInt<type>()
+ll read(){
+	char ch=getchar(); ll x=0,f=1;
+	while(ch<'0'||ch>'9'){ if(ch=='-') f=-1; ch = getchar();}
+	while(ch>='0'&&ch<='9') x = x*10 + (ch^48), ch = getchar();
+	return x*f;
+}
 
 void __print(int x) {cerr << x;}
 void __print(long x) {cerr << x;}
@@ -53,32 +61,38 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define debug(x...)
 #endif
 
+int dp[100004];
+int n;
+int a[100004];
+int func(int ind) {
+    if (ind > n) return 0;
+ 
+    if (dp[ind] != -1) return dp[ind];
+ 
+    int count = 0;
+ 
+    for (int i = ind; i <= n; i += ind) {
+        if (a[i] > a[ind])
+            count = max(count, 1 + func(i));
+    }
+ 
+    return dp[ind] = count;
+}
+ 
 void Solve()
 {
-	int n, m;
-    cin >> n >> m;
-    vector<string> s(n);
 
-    loop cin >> s[i];
-    lop(r,2){
-      	bool ok = true;
+	cin >> n;
+	lp(i,1,n+1){
+		cin >> a[i];
+		dp[i] = -1;
+	}
 
-        loop lop(j,m){ 
-      	    char c = ((i + j + r) % 2 == 0 ? 'R' : 'W');
-      	    if (s[i][j] == c) ok = false;
-        }
-	
-      	if(ok){
-      		cout << "YES" << '\n';
-        	loop{
-				lop(j,m) {
-         		   char c = ((i + j + r + 1) % 2 == 0 ? 'R' : 'W');
-         		   cout << c;
-         		}cout << '\n';
-        	}
-      	  	return;
-      	}
-    }no;
+	int maxi = 1;
+	lp(i,1,n+1){
+		maxi = max(maxi, 1 + func(i));
+	}
+	cout<<maxi<<endl;
 }
 
 int main()
@@ -86,9 +100,9 @@ int main()
  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
  srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 
- int Testcase = 1;
- cin>>Testcase;   
+ int TestCase=1;
+ cin>>TestCase;
 
- while (Testcase--)Solve();
+ while (TestCase--) Solve();
  return 0;
 }
