98bacb404518eb779dec9f80048b8e4ef328cea2
stunnerhash <utyadav07860@gmail.com>
2021-07-06 03:28:26 +0530

    starter file updates

--- .vscode/starter.cpp
+++ .vscode/starter.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define loop           	for(int i = 0; i < n; i++)
-#define lop(i, n)      	for(int i = 0; i < n; i++)
-#define lp(i, k, n)    	for(int i=k;k < n?i < n: i>n;k < n? i+=1: i-=1)
+#define loop           	for (int i = 0; i < n; i++)
+#define lop(i, n)      	for (int i = 0; i < n; i++)
+#define lp(i, k, n)    	for (int i=k;k < n?i < n: i>n;k < n? i+=1: i-=1)
+#define trav(a) 		for (auto it = a.begin();  it != a.end();  it++)
 #define dbg(x)         	{cout<<#x<<"="<<x<<endl;cerr<<#x<<"="<<x<<endl;}
-#define yes(flag)      	cout<<(flag?"YES":"NO")<<endl;
-#define no             	{cout << "NO" <<endl; return;}
-#define clr(x)         	memset(x, 0, sizeof(x))
-#define trav(a)        	for (auto it : a)
-#define all(x)         	x.begin(),x.end()
+#define yes(x)      	cout<<(x?"YES\n":"NO\n")
+#define no             	{cout<< "NO\n"; return;}
+#define clr(x)         	memset (x, 0, sizeof(x))
+#define all(x)         	x.begin(), x.end()
 #define sortall(x)     	sort(all(x))
 #define ll             	long long
 #define pb             	push_back
 #define mp             	make_pair
 #define INF            	(int) 1e9
-#define mod            	INF + 7
+#define mod            	1000000007	
 #define ss             	second
 #define ff             	first
 #define endl           	"\n"
@@ -60,7 +60,7 @@ template <typename T, typename... V>
 #endif
 
 // #define int long long
-const int  N = 1e5+5;
+const int  N = 200005;
 
 void Solve()
 {}
