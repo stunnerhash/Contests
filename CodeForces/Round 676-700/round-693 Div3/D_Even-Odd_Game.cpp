d114b0ed2503b3079b22981dad397fb872a91e36
stunnerhash <utyadav07860@gmail.com>
2021-07-19 01:23:40 +0530

    round-693 Div3 ques-d

--- CodeForces/Round 676-700/round-693 Div3/D_Even-Odd_Game.cpp
+++ CodeForces/Round 676-700/round-693 Div3/D_Even-Odd_Game.cpp
@@ -43,66 +43,34 @@ void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ 
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
 #ifndef ONLINE_JUDGE
 #define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
 #else
 #define debug(x...)
 #endif
 
+#define int long long 
+
 void Solve()
 {
-  int n ;
-  cin>>n;
-  vector<ll> even ;
-  vector<ll> odd;
-  loop{
-       ll  x;
-       cin>>x;
-       if(x%2) odd.pb(x);
-        else even.pb(x);
-  }
-  ll suma=0, sumb =0;
-  int temp = min(even.size(),odd.size());
-    bool flag = (temp == even.size());
-    sort(even.begin(),even.end(),greater<int>());
-    sort(odd.begin(),odd.end(),greater<int>());
-    int i =0,j=0;
-    int x =0;
-    while(i<even.size() and j<odd.size()){
-        //alice turn
-        if(even[i] > odd[j]){
-            suma+= even[i];
-            i++;
-        }
-        else{
-         j++;
-        }
-        x++;
-        //bob turn
-        if(j<odd.size() and i<even.size()){
-            if(even[i] < odd[j]){
-                sumb += odd[j];
-                j++;
-            }
-            else{
-                i++;
-            } 
-            x++;
-        }
-    }
-    if(x%2) i++;
-    while(i<even.size()){
-        suma+=even[i];
-        i+=2;
-    }
-    if(x%2==0) j++;
-    while(j<odd.size()){
-        sumb += odd[j];
-        j+= 2;
-    }
-    if(suma== sumb ) cout<<"Tie\n";
-    else if (suma>sumb) cout<<"Alice\n";
-    else cout<< "Bob\n";
+   	int n; cin>>n;
+	vi a(n); loop cin >> a[i];
+
+	sortall(a);
+	reverse(all(a));
+
+	int A = 0, B = 0;
+
+	loop if ((i + 1) % 2 != 0)
+			if (a[i] % 2 == 0) 
+			 	A = A + a[i];
+		else if (a[i] % 2 != 0) 
+			B = B + a[i];
+
+	if (A > B) cout << "Alice" << endl;
+	else if (A < B) cout << "Bob" << endl;
+	else cout << "Tie" << endl;
 }
 
 int main()
