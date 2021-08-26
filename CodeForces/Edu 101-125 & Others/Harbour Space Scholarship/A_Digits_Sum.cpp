6267b4e65c9528b5f27bc53f186dd2dd629f446c
stunnerhash <utyadav07860@gmail.com>
2021-08-26 23:58:10 +0530

    codeforces harbour space scholarship  quest-abcd

---
+++ CodeForces/Edu 101-125 & Others/Harbour Space Scholarship/A_Digits_Sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main()
+{
+	int Testcase = 1;
+	cin>>Testcase;
+
+	while (Testcase--) {
+		int n; cin>>n;
+		cout<<(n+1)/10<<endl;
+	}
+}
---
+++ CodeForces/Edu 101-125 & Others/Harbour Space Scholarship/C_Penalty.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop           	for (int i = 0; i < n; i++)
+#define lop(i, n)      	for (int i = 0; i < n; i++)
+#define lp(i, k, n)    	for (int i=k;k < n?i < n: i>n;k < n? i+=1: i-=1)
+#define trav(a) 		for (auto it = a.begin();  it != a.end();  it++)
+#define dbg(x)         	{cout<<#x<<"="<<x<<endl;cerr<<#x<<"="<<x<<endl;}
+#define yes(x)      	cout<<(x?"YES\n":"NO\n")
+#define no             	{cout<< "NO\n"; return;}
+#define clear(x)       	memset (x, 0, sizeof(x))
+#define all(x)         	x.begin(), x.end()
+#define sortall(x)    	sort(all(x))
+#define ll             	long long
+#define pb             	push_back
+#define mp             	make_pair
+#define INF            	(int) 1e9
+#define mod            	1000000007	
+#define ss             	second
+#define ff             	first
+#define endl           	"\n"
+typedef pair<int, int> 	pi;
+typedef pair<ll, ll>   	pl;
+typedef vector<int>    	vi;
+typedef vector<ll>     	vl;
+typedef vector<pi>     	vpi;
+typedef vector<vi>     	vvi;
+
+
+void __print(int x)    	        {cerr << x;}
+void __print(long x)   	        {cerr << x;}
+void __print(float x)  	        {cerr << x;}
+void __print(double x) 	        {cerr << x;}
+void __print(unsigned x)       	{cerr << x;}
+void __print(long long x)      	{cerr << x;}
+void __print(long double x)    	{cerr << x;}
+void __print(unsigned ll x)    	{cerr << x;}
+void __print(unsigned long x)  	{cerr << x;}
+void __print(const char *x)    	{cerr << '"' << x << '"';}
+void __print(const string &x)  	{cerr << '"' << x << '"';}
+void __print(char x)           	{cerr << '\''<< x <<'\'';}
+void __print(bool x)           	{cerr <<(x?"true":"false");}
+
+template<typename T, typename V>
+	void __print(const pair<T, V> &x)
+		{cerr << '{'; __print(x.first);cerr<<',';__print(x.second);cerr<<'}';}
+template<typename T>
+	void __print(const T &x) 
+		{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+	void _print()
+		{cerr << "]\n";}
+template <typename T, typename... V>
+	void _print(T t, V... v)
+		{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+// #define int long long
+const int  N = 200005;
+
+void Solve()
+{
+	string s;	cin>>s;
+	int a=0,b=0,c1=0,c2=0,i;
+	for(i = 0;i<10;i++){
+		if (s[i] == '1')
+			if (i % 2 == 0) a++;
+			else b++;
+
+		if (s[i] == '?')
+			if (i % 2 == 0) c1++;
+			else c2++;
+
+		if (a + c1 > b + (10 - i) / 2 || b + c2 > a + (9 - i) / 2) {
+			i++;
+			break;
+		}
+	}
+	cout << i << endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+	int Testcase = 1;
+	cin>>Testcase;
+
+	while (Testcase--) Solve();
+	 return 0;
+}
