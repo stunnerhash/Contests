520a7324eee51868e8c05cb8e0eada558c9d3043
stunnerhash <utyadav07860@gmail.com>
2022-08-20 20:00:19 +0530

    round 815 div2

---
+++ CodeForces/Round 801-825/round 815 div2/B_Interesting_Sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop           	for (int i = 0; i < n; i++)
+#define all(x)         	x.begin(), x.end()
+#define sortall(x)    	sort(all(x))
+#define endl           	"\n"
+typedef vector<int>    	vi;
+
+#define int long long
+void solve()
+{
+	int n; cin>>n;
+	vi a(n); loop cin>>a[i];
+	sortall(a);
+	cout<<a[n-2]+a[n-1]-a[0]-a[1]<<endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+	int tc = 1; cin>>tc;
+
+	while (tc--) solve();
+	return 0;
+}
---
+++ CodeForces/Round 801-825/round 815 div2/C_Corners.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop           	for (int i = 0; i < n; i++)
+#define lop(i, n)      	for (int i = 0; i < n; i++)
+#define lp(i, k, n)    	for (int i=k;k < n?i < n: i>n;k < n? i+=1: i-=1)
+#define trav(a) 		for (auto it = a.begin();  it != a.end();  it++)
+#define yesno(x)      	cout<<(x?"YES\n":"NO\n")
+#define yes				{cout<< "YES\n"; return;}
+#define no             	{cout<< "NO\n"; return;}
+#define all(x)         	x.begin(), x.end()
+#define travauto(a)		for (auto& it:a)
+#define sortall(x)    	sort(all(x))
+#define ll             	long long
+#define pb             	push_back
+#define ss             	second
+#define ff             	first
+#define endl           	"\n"
+typedef pair<int, int> 	pi;
+typedef pair<ll, ll>   	pl;
+typedef vector<int>    	vi;
+typedef vector<ll>     	vl;
+typedef vector<pi>     	vpi;
+typedef vector<vi>     	vvi;
+const ll mod  = 1000000007;
+const ll inf  =	1e9;
+const ll linf =	1e18;
+
+
+void __print(int x)    	        {cerr << x;}
+void __print(long x)   	        {cerr << x;}
+void __print(float x)  	        {cerr << x;}
+void __print(double x) 	        {cerr << x;}
+void __print(unsigned x)       	{cerr << x;}
+void __print(long long x)      	{cerr << x;}
+void __print(long double x)    	{cerr << x;}
+void __print(unsigned ll x)    	{cerr << x;}
+void __print(unsigned long x)  	{cerr << x;}
+void __print(const char *x)    	{cerr << '"' << x << '"';}
+void __print(const string &x)  	{cerr << '"' << x << '"';}
+void __print(char x)           	{cerr << '\''<< x <<'\'';}
+void __print(bool x)           	{cerr <<(x?"true":"false");}
+
+template<typename T, typename V>
+	void __print(const pair<T, V> &x)
+		{cerr << '{'; __print(x.first);cerr<<',';__print(x.second);cerr<<'}';}
+template<typename T>
+	void __print(const T &x) 
+		{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+	void _print()
+		{cerr << "]\n";}
+template <typename T, typename... V>
+	void _print(T t, V... v)
+		{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define int long long
+
+void solve()
+{
+	int n,m; cin>>n>>m;
+	vector<string> a(n);
+	loop cin>>a[i];
+	int cnt = 0, two = 0;
+	loop lop(k,m) cnt += (a[i][k] == '1');
+	lop(i,n) lop(k,m){
+		char c = a[i][k];
+		if(c == '0'){
+			if(i+1<n  and c == a[i+1][k] ) two = 1;
+			if(k+1<m and  c == a[i][k+1] ) two = 1;
+			if(i+1<n and k+1<m and  c == a[i+1][k+1]) two = 1;
+			if(i>0 and k+1<m and c == a[i-1][k+1]) two  = 1; 
+		}
+	}
+	int ans = 0;
+	if (cnt == n * m) ans = cnt - 2;
+	else if(two == 1) ans = cnt;
+	else ans = cnt-1;
+	cout<<ans<<endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+	int tc = 1;
+	cin>>tc;
+
+	while (tc--) solve();
+	return 0;
+}
+
+//I, stunnerhash, hereby declare myself as the owner of this piece of shit code.
