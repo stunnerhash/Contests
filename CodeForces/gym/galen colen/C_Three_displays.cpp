79735c97145491d603f8101648b3e6d00f34b857
stunnerhash <utyadav07860@gmail.com>
2023-01-04 20:03:05 +0530

    cf gym galen colen 1200-1700

---
+++ CodeForces/gym/galen colen/C_Three_displays.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop           	for (int i = 0; i < n; i++)
+#define lop(i, n)      	for (int i = 0; i < n; i++)
+#define lp(i, x, n)    	for (int i = x; i < n; i++)
+#define pool		    for (int i = n-1; i >=0; i--)
+#define return(x)  		{cout<<x<<'\n'; return;}
+#define yes				return("YES")
+#define no             	return("NO")
+#define yesno(x)      	cout<<(x?"YES":"NO")<<'\n'
+#define all(x)         	x.begin(), x.end()
+#define sortall(x)    	sort(all(x))
+#define ll             	long long
+#define pb             	push_back
+#define ss             	second
+#define ff             	first
+#define endl           	'\n'
+typedef pair<int, int> 	pi;
+typedef pair<ll, ll>   	pl;
+typedef vector<int>    	vi;
+typedef vector<ll>     	vl;
+typedef vector<pi>     	vpi;
+typedef vector<vi>     	vvi;
+const ll mod  = 1000000007;
+const ll inf  =	1e9;
+const ll linf =	1e18;
+
+
+void __print(int x)    	        {cerr << x;}
+void __print(long x)   	        {cerr << x;}
+void __print(float x)  	        {cerr << x;}
+void __print(double x) 	        {cerr << x;}
+void __print(unsigned x)       	{cerr << x;}
+void __print(long long x)      	{cerr << x;}
+void __print(long double x)    	{cerr << x;}
+void __print(unsigned ll x)    	{cerr << x;}
+void __print(unsigned long x)  	{cerr << x;}
+void __print(const char *x)    	{cerr << '"' << x << '"';}
+void __print(const string &x)  	{cerr << '"' << x << '"';}
+void __print(char x)           	{cerr << '\''<< x <<'\'';}
+void __print(bool x)           	{cerr <<(x?"true":"false");}
+
+template<typename T, typename V>
+	void __print(const pair<T, V> &x)
+		{cerr << '{'; __print(x.first);cerr<<',';__print(x.second);cerr<<'}';}
+template<typename T>
+	void __print(const T &x) 
+		{int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+	void _print()
+		{cerr << "]\n";}
+template <typename T, typename... V>
+	void _print(T t, V... v)
+		{__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+
+void solve()
+{
+	int n; cin>>n;
+	vector<vl> dp(n,vl(3,1e9));
+	vl s(n); loop cin>>s[i];
+	loop cin>>dp[i][0];
+	loop lop(k,i) if(s[k]<s[i]) dp[i][1] = min(dp[i][1],dp[i][0]+dp[k][0]);
+	loop lop(k,i) if(s[k]<s[i]) dp[i][2] = min(dp[i][2],dp[i][0]+dp[k][1]);
+	
+	int ans = 1e9; loop ans = min(dp[i][2],ans);
+	cout<<(ans==1e9?-1:ans)<<endl;
+}
+signed main() { solve(); }
+
+//I, stunnerhash, hereby declare myself as the owner of this piece of shit code.
