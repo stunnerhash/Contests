157687ad26842d51557f5022ff85db0cc27a6635
utkarsh <utyadav07860@gmail.com>
2023-08-28 12:08:29 +0530

    round 894 div3 abcdef

---
+++ CodeForces/Round 876-900/round 894 div3/F_Magic_Will_Save_the_World.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop           	for (int i = 0; i < n; i++)
+#define lop(i, n)      	for (int i = 0; i < n; i++)
+#define lp(i, x, n)    	for (int i = x; i < n; i++)
+#define pool            for (int i = n-1; i >=0; i--)
+#define returnn(x)      {cout<<x<<'\n'; return;}
+#define yes             returnn("YES")
+#define no              returnn("NO")
+#define yesno(x)        cout<<(x?"YES":"NO")<<'\n'
+#define all(x)         	x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define ll             	long long
+#define pb             	push_back
+#define ss             	second
+#define ff             	first
+#define endl           	'\n'
+typedef pair<int, int> 	pi;
+typedef pair<ll, ll>   	pl;
+typedef vector<int>    	vi;
+typedef vector<ll>     	vl;
+typedef vector<pi>     	vpi;
+typedef vector<vi>     	vvi;
+const ll mod  = 1000000007;
+const ll inf  =	1e9;
+const ll linf =	1e18;
+
+void __print(int x)    	        {cerr << x;}
+void __print(long x)   	        {cerr << x;}
+void __print(float x)  	        {cerr << x;}
+void __print(double x) 	        {cerr << x;}
+void __print(unsigned x)       	{cerr << x;}
+void __print(long long x)      	{cerr << x;}
+void __print(long double x)    	{cerr << x;}
+void __print(unsigned ll x)    	{cerr << x;}
+void __print(unsigned long x)  	{cerr << x;}
+void __print(const char *x)    	{cerr << '"' << x << '"';}
+void __print(const string &x)  	{cerr << '"' << x << '"';}
+void __print(char x)           	{cerr << '\''<< x <<'\'';}
+void __print(bool x)           	{cerr <<(x?"true":"false");}
+
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first);cerr<<',';__print(x.second);cerr<<'}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";} void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define int long long
+
+void solve() {
+    int f,w,n; cin>>f>>w>>n;
+    vi a(n); loop cin>>a[i];
+    int sum = 0; loop sum += a[i];
+
+    vector<bool> dp(sum + 1); dp[0] = true;
+    for (int i = 0; i < n; ++i) 
+        for (int w = sum; w >= a[i]; w--) 
+            dp[w] = dp[w] || dp[w - a[i]];
+
+    int ans = 2e9;
+    for (int i = 0; i <= sum; ++i) 
+        if (dp[i]) ans = min(ans, max((i+w-1)/w, (sum-i+f-1)/f));
+    cout << ans << "\n";
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+	int tc = 1;
+	cin>>tc;
+
+	while (tc--) solve();
+	return 0;
+}
+// If it works... don't touch it.
